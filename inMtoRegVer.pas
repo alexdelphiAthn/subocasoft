d3e14c277512fee82fdb59a2a13d8d042358b10a
Alex Delphi Athns <alejandro.laorden@proton.me>
2025-09-24 12:58:42 +0200

    Add files via upload

---
+++ inMtoRegVer.pas
@@ -0,0 +1,106 @@
+unit inMtoRegVer;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, inMtoGen, cxStyles, cxCustomData, cxGraphics, cxFilter, cxData,
+  cxDataStorage, cxEdit, DB, cxDBData,   DBCtrls,
+  cxNavigator, ExtCtrls, //dxCntner, dxExEdtr, dxEdLib,
+  cxContainer, unidataconn,
+  cxTextEdit, cxGridLevel, cxClasses, cxControls, cxGridCustomView,
+  cxGridCustomTableView, cxGridTableView, cxGridDBTableView, cxGrid,
+  ComCtrls, StdCtrls, Buttons, ADODB, //BetterADODataSet,
+  cxDBEdit,
+  dxSkinsCore, dxSkinBlack, dxSkinBlue, dxSkinCaramel, dxSkinCoffee,
+  dxSkinDarkSide, dxSkinGlassOceans, dxSkiniMaginary, dxSkinLilian,
+  dxSkinLiquidSky, dxSkinLondonLiquidSky, dxSkinMcSkin, dxSkinMoneyTwins,
+  dxSkinOffice2007Black, dxSkinOffice2007Blue, dxSkinOffice2007Green,
+  dxSkinOffice2007Pink, dxSkinOffice2007Silver, dxSkinPumpkin,
+  dxSkinSilver, dxSkinStardust, dxSkinSummer2008, dxSkinsDefaultPainters,
+  dxSkinValentine, dxSkinXmas2008Blue, dxSkinscxPCPainter, cxLookAndFeels,
+  cxLookAndFeelPainters, cxCheckBox, cxLabel, cxDBLabel, cxTimeEdit, cxCalendar, inMtoPrincipal,
+   cxLocalization,
+  cxDataControllerConditionalFormattingRulesManagerDialog, dxDateRanges, MemDS,
+  DBAccess, Uni, dxBevel, cxDBNavigator, dxScrollbarAnnotations;
+
+type
+  TfrmMtoRegVer = class(TfrmMtoGen)
+    unqryRegVer: TUniQuery;
+    dbcGrdDBTabPrinID_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinTIMESTAMP_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinTIPO_EVENTO_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinUSUARIO_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinVERSION_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinDESCRIPCION_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinDATOS_ADICIONALES_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinHASH_ANTERIOR_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinHASH_PROPIO_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinFIRMA_DIGITAL_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinCREATED_AT_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinNRO_FACTURA_LOG: TcxGridDBColumn;
+    dbcGrdDBTabPrinSERIE_FACTURA_LOG: TcxGridDBColumn;
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  private
+    //FDmConn:TdmConn;
+    FException: boolean;
+  public
+    { Public declarations }
+  end;
+  procedure ShowMtoRegVer(Owner       : TComponent);
+
+
+var
+  frmMtoRegVer: TfrmMtoRegVer;
+
+implementation
+
+uses inLibWin;
+
+{$R *.dfm}
+
+procedure ShowMtoRegVer(Owner       : TComponent);
+var
+  FfrmUbi : TfrmMtoRegVer;
+begin
+  if not(IsOpenMDI('Registro VeriFactu', Owner)) then
+  begin
+    Application.CreateForm(TfrmMtoRegVer, FfrmUbi);
+  end;
+end;
+
+procedure TfrmMtoRegVer.FormShow(Sender: TObject);
+begin
+  inherited;
+  if FException then
+  begin
+     PostMessage( Handle, wm_Close, 0, 0 );
+     Exit;
+  end;
+end;
+
+procedure TfrmMtoRegVer.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  unqryRegVer.Close;
+  inherited;
+end;
+
+procedure TfrmMtoRegVer.FormCreate(Sender: TObject);
+begin
+  try
+    with frmopenapp do
+    unqryRegVer.Connection := FDmConn.conUni;
+    dsTablaG.DataSet.Open;
+    inherited;
+  except
+     FException := True;
+  raise;
+
+  end;
+   //pcDetall.ActivePage := tsDatGen;
+end;
+
+end.
+
